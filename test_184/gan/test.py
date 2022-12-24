d17f0ff015c617a5619a85c9c1b4cde1d0336a53
letcodeflow <ghimsoomin@gmail.com>
2022-12-24 20:46:33 +0900

    satur

---
+++ test_184/gan/test.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+tf.debugging.set_log_device_placement(True)
+
+# 텐서 생성
+# a = tf.constant([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
+# b = tf.constant([[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]])
+# c = tf.matmul(a, b)
+
+# print(c)..
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+  # 텐서플로가 첫 번째 GPU만 사용하도록 제한.
+  try:
+    tf.config.experimental.set_visible_devices(gpus[0], 'GPU')
+  except RuntimeError as e:
+    # 프로그램 시작시에 접근 가능한 장치가 설정되어야만 합니다
+    print(e)
